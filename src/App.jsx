35b12113159221a7801b9bcef4291164ea2e552f
Lucas Simidu <lucas.simidu@outlook.com>
2025-08-25 01:19:04 -0300

    feat: main components (app, addtask & tasks)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
-import { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 
 function App() {
-  const [message, setMessage] = useState("Hello world!")
-  const [title, setTitle] = useState("Título padrão");
-  // State (Estado)
   return(
     <div>
-      <h1>{title}</h1>
-      <p>{message}</p>
-      <button onClick={() => {
-        setTitle('Título alterado');
-        setMessage('Mensagem alterada');
-      }}
-      >
-        Mudar mensagem
-      </button>
+      <h1>Gerenciador de Tarefas</h1>
+      <Tasks/>
+      <AddTask/>
     </div>
   )
 }
