c63ce5e22758d216f73bfe94506e18bf620357a3
Lucas Simidu <lucas.simidu@outlook.com>
2025-08-25 00:57:18 -0300

    feat: useState examples

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 function App() {
-  const message = 'Hello World!';
+  const [message, setMessage] = useState("Hello world!")
+  const [title, setTitle] = useState("Título padrão");
+  // State (Estado)
   return(
     <div>
-      <h1>{message}</h1>
+      <h1>{title}</h1>
+      <p>{message}</p>
+      <button onClick={() => {
+        setTitle('Título alterado');
+        setMessage('Mensagem alterada');
+      }}
+      >
+        Mudar mensagem
+      </button>
     </div>
   )
 }
