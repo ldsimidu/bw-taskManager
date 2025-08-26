648c6487bc4839cb0de0f964500c5bc323c73b28
Lucas Simidu <lucas.simidu@outlook.com>
2025-08-26 00:28:40 -0300

    feat: working with props
    using prop with task list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([{
+    id: 1,
+    title: "Jogar The Legend Of Zelda: Tears Of Kingdom",
+    description: "Encontrar Zelda",
+    isCompleted: false
+  }, {
+    id: 2,
+    title: "Jogar The Witcher 3",
+    description: "Salvar Ciri",
+    isCompleted: false
+  }, {
+    id: 3,
+    title: "Assistir Formula 1",
+    description: "Canal: Band, 10h da manhã do sabado",
+    isCompleted: false
+  }]);
+  
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks/>
+        <Tasks tasks={tasks}/>
         <AddTask/>
       </div>
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,12 @@
-function Tasks() {
+function Tasks(props) {
     return (
-        <h1>Tasks</h1>
+        <div>
+            <ul className="space-y-4">
+                {props.tasks.map((task) => (
+                    <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">{task.title}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
