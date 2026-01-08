7fa20062bf77e3d18cd51927485be481d5b9fad3
Lucas Simidu <ldsimidu@gmail.com>
2026-01-08 00:08:11 -0300

    feat: Base add task component

--- src/App.jsx
+++ src/App.jsx
@@ -42,12 +42,13 @@ function App() {
   
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
         <AddTask/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
+        
       </div>
     </div>
   )
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,6 +1,18 @@
 function AddTask() {
     return (
-        <h1>Add Task</h1>
+        <div className="space-y-4 p-6 bg-gray-200 rounded-md shadow flex flex-col">
+            <input 
+                type="text" 
+                placeholder="Digite o título da tarefa" 
+                className="bg-white border border-slate-300 outline-slate-400 px-4 py-2 rounded-md" 
+            />
+            <input 
+                type="text" 
+                placeholder="Digite a descrição da tarefa" 
+                className="bg-white border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            />
+            <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+        </div>
     )
 }
 
