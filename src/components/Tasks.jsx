d2614c7ae34bcbaff8353ca0bbb01d11b6f23493
Lucas Simidu <ldsimidu@gmail.com>
2026-01-08 00:07:49 -0300

    fix: task component color

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,7 +2,7 @@ import { ChevronRightIcon, Trash2  } from "lucide-react";
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
     return (
-        <ul className="space-y-4 pt-5 bg-gray-100 p-6 rounded-md shadow">
+        <ul className="space-y-4 pt-5 bg-gray-200 p-6 rounded-md shadow">
             {tasks.map((task) => (
                 <li key={task.id} className="flex gap-2">
                     <button 
